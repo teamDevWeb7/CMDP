bee2502adf33c5c764ba50d9673b85745d392c03
teamDevWeb7 <suray.amelyne@gmail.com>
2023-03-10 14:13:56 +0100

    page 2 devis avec rep dans tableau

--- public/assets/js/devis.js
+++ public/assets/js/devis.js
@@ -58,6 +58,150 @@ p[3].addEventListener(
 
 
 
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+// deuxieme page, recueil besoins
+let index;
+let besoins=[];
+let i;
+
+
+// revetements
+p[4].addEventListener(
+    'click',
+    (event)=>{
+        if(span[3].getAttribute('class')==='R2'){
+            span[3].setAttribute("class", "selecte");
+            besoins.push(span[3].innerText);
+        }else{
+            span[3].setAttribute("class", "R2");
+
+            // pr trouver index
+            let retrouver= (element)=> element === span[3].innerText;
+            index=besoins.findIndex(retrouver);
+
+            // supprimer du tableau
+            besoins.splice(index, 1);
+        }
+    } 
+)
+// menuiseries
+p[5].addEventListener(
+    'click',
+    (event)=>{
+        if(span[4].getAttribute('class')==='R2'){
+            span[4].setAttribute("class", "selecte");
+            besoins.push(span[4].innerText);
+        }else{
+            span[4].setAttribute("class", "R2");
+
+            // pr trouver index
+            let retrouver= (element)=> element === span[4].innerText;
+            index=besoins.findIndex(retrouver);
+            
+            // supprimer du tableau
+            besoins.splice(index, 1);
+        }
+    } 
+)
+// Isolation
+p[6].addEventListener(
+    'click',
+    (event)=>{
+        if(span[5].getAttribute('class')==='R2'){
+            span[5].setAttribute("class", "selecte");
+        }else{
+            span[5].setAttribute("class", "R2");
+        }
+    } 
+)
+// toiture
+p[7].addEventListener(
+    'click',
+    (event)=>{
+        if(span[6].getAttribute('class')==='R2'){
+            span[6].setAttribute("class", "selecte");
+        }else{
+            span[6].setAttribute("class", "R2");
+        }
+    } 
+)
+// construction
+p[8].addEventListener(
+    'click',
+    (event)=>{
+        if(span[7].getAttribute('class')==='R2'){
+            span[7].setAttribute("class", "selecte");
+        }else{
+            span[7].setAttribute("class", "R2");
+        }
+    } 
+)
+// sdb
+p[9].addEventListener(
+    'click',
+    (event)=>{
+        if(span[8].getAttribute('class')==='R2'){
+            span[8].setAttribute("class", "selecte");
+        }else{
+            span[8].setAttribute("class", "R2");
+        }
+    } 
+)
+// extérieur
+p[10].addEventListener(
+    'click',
+    (event)=>{
+        if(span[9].getAttribute('class')==='R2'){
+            span[9].setAttribute("class", "selecte");
+        }else{
+            span[9].setAttribute("class", "R2");
+        }
+    } 
+)
+// cuisine
+p[11].addEventListener(
+    'click',
+    (event)=>{
+        if(span[10].getAttribute('class')==='R2'){
+            span[10].setAttribute("class", "selecte");
+        }else{
+            span[10].setAttribute("class", "R2");
+        }
+    } 
+)
+// autre
+p[12].addEventListener(
+    'click',
+    (event)=>{
+        if(span[11].getAttribute('class')==='R2'){
+            span[11].setAttribute("class", "selecte");
+        }else{
+            span[11].setAttribute("class", "R2");
+        }
+    } 
+)
+
+
+
+// faire boucle if class = selecte dans tab
+// function recueil(){
+//     let i;
+//     let besoin=[span[3], span[4], span[5], span[6], span[7], span[8], span[9], span[10], span[11]];
+//     let taille=besoin.length;
+//     let besoins;
+
+//     for(i=0; i<taille; i++){
+//         if(besoin[i].style.class="selecte"){
+//             alert('jojo');
+//             besoins+=besoin[i]+' ';
+//         }
+//         alert(besoins);
+//     }
+// }
+
+
 
 
 
--- public/assets/js/swipe.js
+++ public/assets/js/swipe.js
@@ -3,7 +3,39 @@
 let para1=document.getElementById('p1');
 let para2=document.getElementById('p2');
 
+let btnPreced=document.querySelectorAll('button');
+let div=document.querySelectorAll('div');
+
+// suivant
 function swip(){
-    para1.style.left="-100vw";
-    para2.style.left="0";
+    // si je suis sur page 1 et que je vais sur page 2
+
+        para1.removeAttribute('class');
+        para1.style.left='-100vw';
+
+        para2.removeAttribute('class');
+        para2.setAttribute('class', 'left0');
+        // para2.style.left='0';
+
+        div[3].removeAttribute('class');
+        div[3].setAttribute('class', 'pts1');
+        div[4].setAttribute('class', 'pts2 bigger');
+}
+
+// retour en arriere
+function swi(){
+    // si je suis sur page 2 et que je retourne sur page 1
+    if(para2.getAttribute("class")==="left0"){
+        para1.style.left="0";
+        para2.setAttribute("class", "left");
+
+        div[4].removeAttribute('class');
+        div[4].setAttribute('class', 'pts2');
+        div[3].setAttribute('class', 'pts1 bigger');
+    }
+}
+
+// fonction suivant de lapage 2 vers 3
+function swipo(){
+    alert('hamburger');
 }
