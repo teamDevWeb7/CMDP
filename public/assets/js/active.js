c5951c1ae26bfaf2e488be275116fd9bb744762b
teamDevWeb7 <suray.amelyne@gmail.com>
2023-03-15 09:32:56 +0100

    page active

--- public/assets/js/active.js
+++ public/assets/js/active.js
@@ -1,21 +1,37 @@
-li=document.querySelectorAll('header li');
-// taille=li.length;
-chemin =window.location.pathname;
-array={
-   "/":li[0],
-   "/user/aPropos":li[1],
-   "/user/chantiers":li[2],
-   "/user/devis":li[3],
-   "/user/faq":li[4],
-   "/user/contact":li[5]
+// pour que le a de la page active soit différent des autres
+
+let a=document.querySelectorAll('header li a');
+
+// tableau associatif, clé est le pathname, value est le a
+const array={
+   "/":a[0],
+   "/user/aPropos":a[1],
+   "/user/chantiers":a[2],
+   "/user/devis":a[3],
+   "/user/FAQ":a[4],
+   "/user/contact":a[5]
 }
 
-document.addEventListener(
-    'onload',
+
+window.addEventListener(
+   // event sur page qd chargée
+    'load',
     (event)=>{
-      array.forEach((element, index) => {
-         if(chemin==index){
-            element.style.color='red';
+      // sort le pathname de la page active
+      // nom que j'ai définit dans mes routes
+      let chemin = window.location.pathname;
+
+      // manip en plus pour tableau associatif -> Object.key(...)
+      // boucle forEach sur ts les éléments du tableau
+      Object.keys(array).forEach(function(key){
+         // si key du tab asso == pathname page active
+         if(key == chemin){
+            // console.log(array[key]);
+            // console.log(array[key].innerText);
+
+            // on lui met une class
+            // ne fonctionne pas avec .style.color="..."
+            array[key].setAttribute('class', 'pageActive');
          }
-      });
+      })
     } )
