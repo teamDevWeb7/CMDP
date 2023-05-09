a0d563ff60d7cce26fd31d46867e7884bdf302d8
teamDevWeb7 <suray.amelyne@gmail.com>
2023-05-09 11:15:39 +0200

    input search

--- App/Search/SearchModule.php
+++ App/Search/SearchModule.php
@@ -6,6 +6,7 @@ use Core\Framework\AbstractClass\AbstractModule;
 use Core\Framework\Router\Router;
 use Doctrine\ORM\EntityManager;
 use GuzzleHttp\Psr7\ServerRequest;
+use Model\Entity\Chantier;
 use Model\Entity\Prospect;
 use Psr\Container\ContainerInterface;
 
@@ -13,42 +14,58 @@ class SearchModule extends AbstractModule{
     private ContainerInterface $container;
     private Router $router;
     private $prospRepo;
+    private $chantRepo;
 
     public function __construct(ContainerInterface $container)
     {
         $this->container=$container;
         $this->prospRepo=$this->container->get(EntityManager::class)->getRepository(Prospect::class);
+        $this->chantRepo=$this->container->get(EntityManager::class)->getRepository(Chantier::class);
 
         $this->router=$container->get(Router::class);
 
         $this->router->get('/ajax/prospect/{needle}', [$this, 'findMesProspects'], 'ajaxProsp');
+
+        $this->router->get('/ajax/chantier/{needle}', [$this, 'findMesChantiers'], 'ajaxChant');
     }
 
 
 
-    // public function findMesProspects(ServerRequest $request){
-    //     $data=$request->getAttribute('needle');
-    //     // coucou=alias
-    //     $results=$this->prospRepo->createQueryBuilder('coucou')
-    //     // LIKE DQL
-    //                                 ->where('coucou.nom LIKE :needle')
-    //                                 ->setParameter(':needle', '%'.$data.'%')
-    //                                 ->getQuery()
-    //                                 ->getResult();
+    public function findMesProspects(ServerRequest $request){
+        $data=$request->getAttribute('needle');
+        // coucou=alias
+        $results=$this->prospRepo->createQueryBuilder('coucou')
+        // LIKE DQL
+                                    ->where('coucou.nom LIKE :needle')
+                                    ->setParameter(':needle', '%'.$data.'%')
+                                    ->getQuery()
+                                    ->getResult();
 
-    //     $reponse='';
-    //     $results=array_unique($results, SORT_REGULAR);
-    //     foreach($results as $result){
-    //         $prospIds=$result->ProspIds();
-    //         // $reponse .= '<p><a href="'.$this->router->generateUri('prospect', ['id'=>$id]).">".$result->getNom().'</a></p>';
-    //         foreach($prospIds as $id){
-    //             $reponse .= '<p><a href="'.$this->router->generateUri('prospect', ['id'=>$id]).">".$result->getNom().'</a></p>';
-    //         }
+        $reponse='';
+        $results=array_unique($results, SORT_REGULAR);
+        foreach($results as $result){
+            $reponse.="<p><a href=".$this->router->generateUri('prospect', ['id'=>$result->getId()]).">".$result->getNom()."</a></p>";
+        }
+        return $reponse;
+    }
 
+    public function findMesChantiers(ServerRequest $request){
+        $data=$request->getAttribute('needle');
+        // coucou=alias
+        $results=$this->chantRepo->createQueryBuilder('cc')
+        // LIKE DQL
+                                    ->where('cc.nomChantier LIKE :needle')
+                                    ->setParameter(':needle', '%'.$data.'%')
+                                    ->getQuery()
+                                    ->getResult();
 
-    //     }
-    //     return $reponse;
-    // }
+        $reponse='';
+        $results=array_unique($results, SORT_REGULAR);
+        foreach($results as $result){
+            $reponse.="<p><a href=".$this->router->generateUri('adminChantier', ['id'=>$result->getId()]).">".$result->getNomChantier()."</a></p>";
+        }
+        return $reponse;
+    }
 
 
 }
--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -17,3 +17,24 @@ function searcher(){
     }
     
 }
+
+function input(){
+    const needle = document.querySelector('#searche').value;
+    const divettee = document.querySelector('#azert');
+
+    if(needle.length>2){
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', '/ajax/chantier/'+needle);
+        xhr.responseType='text';
+        xhr.onload=function(){
+            divettee.setAttribute('style', 'display:block;');
+            divettee.innerHTML=xhr.responseText;
+            console.log(xhr.responseText);
+        };
+        xhr.send();
+        console.log(needle);
+    }else{
+        divettee.setAttribute('style', 'display:none;');
+    }
+    
+}
