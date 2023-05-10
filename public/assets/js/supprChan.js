e7dee6e254c862f3ee5a1fc9677c64469ca8425f
teamDevWeb7 <suray.amelyne@gmail.com>
2023-05-10 09:01:59 +0200

    modale chantier

--- public/assets/js/supprChan.js
+++ public/assets/js/supprChan.js
@@ -6,46 +6,60 @@ let btnSpr=document.querySelectorAll('#tof button');
 let mod=document.querySelector('#supprChan');
 
 
-let content=[document.querySelector('h1'), document.querySelector('#att'), document.querySelector('#tof'), document.querySelector('p'), document.querySelector('.opts')];
+let content=[document.querySelector('h1'), document.querySelector('#att'), document.querySelector('p'), document.querySelector('.opts')];
+let arts=document.querySelectorAll('#tof article');
 
 
-function shutDown2(e){
+function shutDown2(){
     mod.classList.remove('attaque');
     mod.classList.add('agentDormant');
-    for(let j=0; j<5; j++){
+    for(let j=0; j<4; j++){
         content[j].classList.remove('blur');
     }
+    arts.forEach(element=>element.classList.remove('blur'));
 
 }
 
 function supprChan(){
     mod.classList.remove('agentDormant');
     mod.classList.add('attaque');
-    for(let j=0; j<5; j++){
+    for(let j=0; j<4; j++){
         content[j].classList.add('blur');
     }
+    arts.forEach(element=>element.classList.add('blur'));
 }
 
 
 
-console.log(modale);
-console.log(p)
 document.addEventListener('click',
     (e)=>{
-        if(e.target.tagName=='P'){
-            console.log(e.target)
+        if(e.target.className=='suppress'){
             for(let i=0; i<trSize; i++){
                 if(e.target==p[i]){
-                    console.log(modale[i]);
                     modale[i].classList.remove('agentDormant');
                     modale[i].classList.add('attaque');
 
                 }
             }
-            for(let j=0; j<5; j++){
+            for(let j=0; j<4; j++){
                 content[j].classList.add('blur');
             }
+            arts.forEach(element=>element.classList.add('blur'));
         }
 
     }
 )
+
+function shutDown3(e){
+    for(let i=0; i<trSize; i++){
+        if(e.target==btnSpr[i]){
+            modale[i].classList.remove('attaque');
+            modale[i].classList.add('agentDormant');
+            for(let j=0; j<4; j++){
+                content[j].classList.remove('blur');
+            }
+            arts.forEach(element=>element.classList.remove('blur'));
+
+        }
+    }
+}
