cc1346522c73d5284ca8114f3b0cc266ed7f231f
teamDevWeb7 <suray.amelyne@gmail.com>
2023-05-26 09:43:15 +0200

    T sur session

--- App/Admin/action/AdminAction.php
+++ App/Admin/action/AdminAction.php
@@ -299,4 +299,11 @@ class AdminAction{
     
         return $this->redirect('pageMessages');
     }
+
+    // $minutesBeforeSessionExpire=30;
+    // if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > ($minutesBeforeSessionExpire*60))) {
+    //     session_unset();     // unset $_SESSION   
+    //     session_destroy();   // destroy session data  
+    // }
+    // $_SESSION['LAST_ACTIVITY'] = time(); // update last activity
 }
