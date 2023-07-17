2cad5b487db9b107c67dbc086b7bf0959fae7619
teamDevWeb7 <suray.amelyne@gmail.com>
2023-07-17 10:20:47 +0200

    reglage css

--- App/Chantier/action/ChantierAction.php
+++ App/Chantier/action/ChantierAction.php
@@ -185,16 +185,6 @@ class ChantierAction{
         if($method=='POST'){
             $data=$request->getParsedBody();
 
-            
-
-            // if($request->getUploadedFiles()['img']->getSize()>=2047674){
-            //     $this->toaster->makeToast("La taille de l'image doit être inférieure à 2MO", Toaster::ERROR);
-            //     return $this->redirect('adminChantier', ["id"=>$id]);
-            // }else{
-            //     $file=$request->getUploadedFiles()['img'];
-            // }
-
-
             $file=$request->getUploadedFiles()['img'];
             
 
